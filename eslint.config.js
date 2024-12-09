b52f1f8506cfcbb9770e3472484140261efb6e0f
suhyun5252 <o52o.suhyun@gmail.com>
2024-12-09 10:16:15 +0900

    [feat] 프로젝트 기본 라이브러리 및 구성 설정

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  // dist 폴더는 검사 제외
+  { ignores: ["dist"] },
+  {
+    // 검사할 파일 확장자
+    files: ["**/*.{js,jsx}"],
+    // 언어 옵션
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    // React 버전 설정
+    settings: { react: { version: "18.3" } },
+    // 플러그인 설정
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh, // React Refresh 규칙 플러그인
+      prettier, // Prettier 플러그인
+    },
+    // 규칙 정의
+    rules: {
+      ...js.configs.recommended.rules, // 기본 JavaScript 권장 규칙
+      ...react.configs.recommended.rules, // React 권장 규칙
+      ...react.configs["jsx-runtime"].rules, // JSX Runtime 규칙
+      ...reactHooks.configs.recommended.rules, // React Hooks 권장 규칙
+      "react/jsx-no-target-blank": "off", // target="_blank" 관련 규칙 비활성화
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ], // React Fast Refresh 규칙
+      "prettier/prettier": "warn", // Prettier 규칙 (포매팅 오류를 에러로 표시)
+    },
+  },
+];
